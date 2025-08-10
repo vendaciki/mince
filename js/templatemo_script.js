845b0bb1776ef505189b3a74897ab296e088b6e2
Vendula Martinková <vendaciki@seznam.cz>
2025-08-10 07:02:33 +0200

    už na tom makám

---
+++ js/templatemo_script.js
@@ -0,0 +1,10 @@
+/* NAVBAR */
+$(document).ready( function() {        
+	$('.templatemo-sidebar-menu li.sub a').click(function(){
+		if($(this).parent().hasClass('open')) {
+			$(this).parent().removeClass('open');
+		} else {
+			$(this).parent().addClass('open');
+		}
+	});  
+}); 
